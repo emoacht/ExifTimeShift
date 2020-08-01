7b6919c09d482e2ec577a4fd9a958f6d1c31e1d8
emoacht <emotom@pobox.com>
2020-08-02 00:20:38 +0900

    Increment minor version

--- Source/ExifDateEditor/Properties/AssemblyInfo.cs
+++ Source/ExifDateEditor/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Exif Date Editor")]
-[assembly: AssemblyCopyright("Copyright © 2017-2018 emoacht")]
+[assembly: AssemblyCopyright("Copyright © 2017-2020 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
